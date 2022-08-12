e73b9699a90f601de1aa0e669e7dfb76954c2f96
jessicaUP <uphoff.jessica@gmail.com>
2022-08-12 14:39:51 -0400

    Add Character Index student/staff functionality

--- src/components/CharactersIndex.vue
+++ src/components/CharactersIndex.vue
@@ -2,14 +2,25 @@
 import { RouterLink } from 'vue-router';
 import { storeToRefs } from 'pinia';
 import { useCharacterStore } from '../stores/characters';
+import { useMapStore } from '../stores/map';
 
 const { staff, students } = storeToRefs(useCharacterStore());
-let currentPage = 'students';
 
+const { index } = storeToRefs(useMapStore());
+const { changeIndex, updateCharacter } = useMapStore();
+
+ 
 </script>
 
 <template>
-    <div class="character-wrap">
+  <div class="index-wrap">
+    <button class="students" @click="changeIndex('students')">Students</button>
+    <button class="staff" @click="changeIndex('staff')">Staff</button>
+    <div class="character-wrap" v-if="index.group === 'students'">
       <p v-if="students" v-for="character in students" :key="character.name">{{ character.name }}</p>
     </div>
+    <div class="character-wrap" v-if="index.group === 'staff'">
+      <p v-if="staff" v-for="character in staff" :key="character.name">{{ character.name }}</p>
+    </div>
+  </div>
 </template>
