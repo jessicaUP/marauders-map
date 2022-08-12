6c52d921aecc6adae28d06e94dbb5c3b2fa143d1
jessicaUP <uphoff.jessica@gmail.com>
2022-08-12 16:20:36 -0400

    Style Pentagon

--- src/components/CharacterShow.vue
+++ src/components/CharacterShow.vue
@@ -12,17 +12,42 @@ updateCharacter('Harry Potter', 'students');
 </script>
 
 <template>
-  <div class="character-wrap" v-if="character">
-    <h2 key="character.name">{{ character.name }}</h2>
-    <p>{{character.dateOfBirth}}</p>
-    <p>{{character.house}}</p>
-    <p>{{character.gender}}</p>
-    <p>{{character.eyeColour}}</p>
-    <p>{{character.hairColour}}</p>
+  <div class="pentagon">
+    <div class="character-wrap" v-if="character">
+      <h2 key="character.name">{{ character.name }}</h2>
+      <p>{{character.dateOfBirth}}</p>
+      <p>{{character.house}}</p>
+      <p>{{character.gender}}</p>
+      <p>{{character.eyeColour}}</p>
+      <p>{{character.hairColour}}</p>
+    </div>
   </div>
 </template>
 
 <style>
+
+  .pentagon {
+    display: flex;
+    position: relative;
+    top: 150px;
+    width: 189px;
+    box-sizing: content-box;
+    border-width: 175px 63px 0;
+    border-style: solid;
+    border-color: rgb(10, 88, 93) transparent;
+  }
+
+  .pentagon:before {
+    content: "";
+    position: absolute;
+    height: 0;
+    width: 0;
+    top: -297.5px;
+    left: -63px;
+    border-width: 0 157.5px 122.5px;
+    border-style: solid;
+    border-color: transparent transparent rgb(10, 88, 93);
+  }
 
 
 </style>
