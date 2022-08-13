ccdd866ab10b9b1fd3b3a59ad2d12a51b90bd7dc
jessicaUP <uphoff.jessica@gmail.com>
2022-08-13 10:46:12 -0400

    Clean up

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <script setup>
 import { useCharacterStore } from './stores/characters'
 import { RouterView } from 'vue-router';
-import { createDOMCompilerError } from '@vue/compiler-dom';
 
 
 // const { staff, students, character, loading, errors } = storeToRefs(useCharacterStore());
 const { fetchCharacters } = useCharacterStore();
 fetchCharacters();
 
-
-// if (params) {
-//   openMap();
-
-//   updateCharacter(params)
-
-// }
 
 
 </script>
