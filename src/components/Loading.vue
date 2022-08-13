d85a7692a08270d5caf196276d5090562c016774
jessicaUP <uphoff.jessica@gmail.com>
2022-08-13 10:52:42 -0400

    Created Loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { storeToRefs } from 'pinia';
+import { useCharacterStore } from '../stores/characters';
+
+
+const { staff, students } = storeToRefs(useCharacterStore());
+
+const { index } = storeToRefs(useMapStore());
+const { changeIndex } = useMapStore();
+
+</script>
+
+<template>
+
+</template>
+
+<style>
+
+</style>
