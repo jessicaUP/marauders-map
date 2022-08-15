4763e4358fa3d956c574ae248bbf326c7ecafcd8
jessicaUP <uphoff.jessica@gmail.com>
2022-08-15 11:54:47 -0400

    Added public path to config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: '/marauders-map/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
